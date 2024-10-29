f3afd8ae520d14b83d2ff7c9b83e18d832ebd76a
Riya Awal <reeyaawal123@gmail.com>
2024-10-29 22:44:20 +0545

    Nodejs MySQL Connection

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 //import
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mySqlPool = require("./config/db");
+
+//configure dotenv
+dotenv.config();
 //rest obj
 const app = express();
 
 //middlewares
-app.use(express.json());
+app.use(express.json()); //to receive json data
 app.use(bodyParser.urlencoded({ extended: false }));
 //routes
 app.use("/api/v1/user", require('./routes/userRoutes'))
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-//listener
-app.listen(PORT, () => {
-    console.log(`Server running on PORT =${PORT}`);
+//conditionally listen
+mySqlPool.query('SELECT 1').then(() => {
+    //MySql
+    console.log('MySql DB Connected');
+    //listener
+    app.listen(PORT, () => {
+        console.log(`Server running on PORT = ${process.env.PORT}`);
+    })
+
 })
